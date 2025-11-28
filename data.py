c8f73469caf943c68f89f2988623ea06c02b1df7
cameronbostic <cameronhudson541@gmail.com>
2025-11-28 14:38:58 -0600

    sprint 8 project changes

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 URBAN_ROUTES_URL = 'https://cnt-ef5a4a49-1e87-4e95-94dc-a2b33f0fa87c.containerhub.tripleten-services.com'
 ADDRESS_FROM = "East 2nd Street,601"
 ADDRESS_TO = "13001 1st Street"
-PHONE_NUMBER = "+1 123 123 12 12"
-CARD_NUMBER = "1234 5678 9100"
-CARD_CODE = "1111"
+PHONE_NUMBER = "+1 123 123 1212"
+CARD_NUMBER = "1234 5678 9123 4567"
+CARD_CODE = "111"
 MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -2,61 +2,68 @@ import data
 import helpers
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
+from pages import UrbanRoutesPage
 
 class TestUrbanRoutes:
 
     @classmethod
     def setup_class(cls):
-        # do not modify - we need additional logging enabled in order to retrieve phone confirmation code
+        # do not modify - logging enabled for phone confirmation retrieval
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
         cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
 
-        # URL reachability check moved inside setup_class
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print("URBAN_ROUTES_URL reachable")
         else:
             print("URBAN_ROUTES_URL not reachable")
 
+        cls.driver.get(data.URBAN_ROUTES_URL)
+        cls.page = UrbanRoutesPage(cls.driver)
+
     def test_set_route(self):
         # Add in S8
-        print("function created for set route")
-        pass
+        self.page.set_route(data.FROM_ADDRESS, data.TO_ADDRESS)
+        assert self.page.driver.find_element(*self.page.from_field).get_attribute("value") == data.FROM_ADDRESS
+        assert self.page.driver.find_element(*self.page.to_field).get_attribute("value") == data.TO_ADDRESS
 
     def test_select_plan(self):
         # Add in S8
-        print("function created for select plan")
-        pass
+        self.page.click_call_taxi()
+        self.page.select_supportive_plan()
+        plan = self.page.driver.find_element(*self.page.supportive_plan)
+        assert "active" in plan.get_attribute("class")
 
     def test_fill_phone_number(self):
         # Add in S8
-        print("function created for fill phone number")
-        pass
+        sms_code = helpers.retrieve_phone_code(self.page.driver)
+        self.page.fill_phone_number(data.PHONE_NUMBER, sms_code)
+        assert self.page.driver.find_element(*self.page.phone_field).get_attribute("value") == data.PHONE_NUMBER
 
     def test_fill_card(self):
         # Add in S8
-        print("function created for fill card")
-        pass
+        self.page.add_card(data.CARD_NUMBER, data.CARD_CVV)
+        payment_method = self.page.driver.find_element_by_class_name("payment-method").text
+        assert payment_method == "Card"
 
     def test_comment_for_driver(self):
         # Add in S8
-        print("function created for comment for driver")
-        pass
+        self.page.add_comment_for_driver(data.DRIVER_COMMENT)
+        assert self.page.driver.find_element(*self.page.comment_field).get_attribute("value") == data.DRIVER_COMMENT
 
     def test_order_blanket_and_handkerchiefs(self):
         # Add in S8
-        print("function created for ordering blanket and handkerchiefs")
-        pass
+        assert self.page.order_blanket_and_handkerchiefs() is True
 
     def test_order_2_ice_creams(self):
-        for i in range(2):
-            # Add in S8
-            pass
+        # Add in S8
+        count = self.page.order_ice_creams(2)
+        assert count == "2"
 
     def test_car_search_model_appears(self):
         # Add in S8
-        print("function created for car search model appears")
-        pass
+        self.page.add_comment_for_driver(data.DRIVER_COMMENT)
+        assert self.page.car_search_modal_displayed() is True
 
     @classmethod
     def teardown_class(cls):
