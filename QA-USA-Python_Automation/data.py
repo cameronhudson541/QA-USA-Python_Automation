191210c12ff53bff1dcae58e5bf24d23f8b3d0bd
cameronbostic <cameronhudson54@yahoo.com>
2025-12-08 01:53:20 -0600

    Resubmission

--- QA-USA-Python_Automation/data.py
+++ QA-USA-Python_Automation/data.py
@@ -1,5 +1,4 @@
-# File with constants
-URBAN_ROUTES_URL = 'https://cnt-8943f435-9808-4db7-ace5-d1039d5f01c6.containerhub.tripleten-services.com/'
+URBAN_ROUTES_URL = 'https://cnt-d18133b8-8081-4b40-a00c-29e9c39ca9c2.containerhub.tripleten-services.com'
 ADDRESS_FROM = "East 2nd Street,601"
 ADDRESS_TO = "13001 1st Street"
 PHONE_NUMBER = "+1 123 123 1212"
--- QA-USA-Python_Automation/main.py
+++ QA-USA-Python_Automation/main.py
@@ -1,5 +1,6 @@
 import data
 import helpers
+from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from pages import UrbanRoutesPage
 class TestUrbanRoutes:
@@ -18,99 +19,96 @@ class TestUrbanRoutes:
     def test_set_route(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page =UrbanRoutesPage(self.driver)
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
         assert page.get_from_address()==data.ADDRESS_FROM
         assert page.get_to_address() == data.ADDRESS_TO
 
     def test_select_plan(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page = UrbanRoutesPage(self.driver)
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
-        page.click_call_taxi_button()
-        page.click.supportive()
-        assert page.click_supportive() == True
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
+        page.call_a_taxi_button()
+        page.select_supportive_plan()
+        assert page.select_supportive_plan() == True
 
 
     def test_fill_phone_number(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page = UrbanRoutesPage(self.driver)
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
-        page.click_call_taxi_button()
-        page.click.supportive()
-        page.write_from_address(data.PHONE_NUMBER)
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
+        page.call_a_taxi_button()
+        page.fill_phone_number()
+        page.select_supportive_plan()
+        page.write_phone_number(data.PHONE_NUMBER)
         page.click_next()
         page.write_code(helpers.retrieve_phone_code(self.driver))
         page.click_confirm()
-        assert page.phone_number_field() == data.PHONE_NUMBER
+        assert page.get_phone_number_displayed() == data.PHONE_NUMBER
 
     def test_fill_card(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page = UrbanRoutesPage(self.driver)
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
-        page.click_call_taxi_button()
-        page.click.supportive()
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
+        page.call_a_taxi_button()
+        page.select_supportive_plan()
         page.click_payment_method()
         page.click_add_card()
         page.fill_card_number()
         page.fill_card_code()
         page.click_link()
-        assert page.card_number_field() == data.CARD_NUMBER
+        assert page.get_payment_method_displayed() == data.CARD_NUMBER
 
 
     def test_comment_for_driver(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page = UrbanRoutesPage(self.driver)
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
-        page.click_call_taxi_button()
-        page.click.supportive()
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
+        page.call_a_taxi_button()
+        page.select_supportive_plan()
         page.fill_driver_comment()
         assert page.driver_comment_field() == data.MESSAGE_FOR_DRIVER
 
-    def test_order_blanket_and_handkerchiefs(self, checkbox):
+    def test_order_blanket_and_handkerchiefs(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page = UrbanRoutesPage(self.driver)
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
-        page.click_call_taxi_button()
-        page.click.supportive()
-        page.click_blanket_and_handkerchifs()
-        assert checkbox.get_property("checked") is True
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
+        page.call_a_taxi_button()
+        page.select_supportive_plan()
+        page.click_blanket_and_handkerchiefs()
+        assert page.is_blanket_and_handkerchiefs_selected() == True
 
     def test_order_2_ice_creams(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page = UrbanRoutesPage(self.driver)
 
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
-        page.click_call_taxi_button()
-        page.click_supportive()
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
+        page.call_a_taxi_button()
+        page.select_supportive_plan()
 
         for _ in range(2):
             page.order_ice_creams()
 
-        count = page.get_ice_cream_count()
+        count = int(page.get_ice_cream())
         assert count == 2, f"Expected 2 ice creams but got {count}"
 
     def test_car_search_model_appears(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         page = UrbanRoutesPage(self.driver)
         page = UrbanRoutesPage(self.driver)
-        page.write_from_address(data.ADDRESS_FROM)
-        page.write_to_address(data.ADDRESS_TO)
-        page.click_call_taxi_button()
-        page.click.supportive()
-        page.write_from_address(data.PHONE_NUMBER)
-        page.click_next()
-        page.write_code(helpers.retrieve_phone_code(self.driver))
-        page.click_confirm()
-        page.fill_driver_message()
+        page.input_from_address(data.ADDRESS_FROM)
+        page.input_to_address(data.ADDRESS_TO)
+        page.call_a_taxi_button()
+        page.select_supportive_plan()
+        page.fill_driver_comment()
         page.click_order_button()
-        assert page.is_car_modal_displayed() == True
+        assert page.test_car_search_model_appears() == True
 
 
     @classmethod
