340c54c01d264c4427f9a86cc55e379d97783c40
cameronhudson541 <cameronhudson541@gmail.com>
2025-12-08 12:03:45 -0600

    Delete QA-USA-Python_Automation directory

--- QA-USA-Python_Automation/data.py
+++
@@ -1,7 +0,0 @@
-URBAN_ROUTES_URL = 'https://cnt-d18133b8-8081-4b40-a00c-29e9c39ca9c2.containerhub.tripleten-services.com'
-ADDRESS_FROM = "East 2nd Street,601"
-ADDRESS_TO = "13001 1st Street"
-PHONE_NUMBER = "+1 123 123 1212"
-CARD_NUMBER = "1234 5678 9123 4567"
-CARD_CODE = "1111"
-MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
