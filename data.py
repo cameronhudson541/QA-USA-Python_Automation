995e45f21ac84806625da938bdf4c36f63d585ab
cameronhudson541 <cameronhudson541@gmail.com>
2025-12-08 12:04:09 -0600

    Update URLs and address details in data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-URBAN_ROUTES_URL = 'https://cnt-55f5a351-4ad4-4211-9a42-28a1abd9058d.containerhub.tripleten-services.com'
+URBAN_ROUTES_URL = 'https://cnt-a84df0dc-98b1-43c0-992e-d45d6f505104.containerhub.tripleten-services.com'
 ADDRESS_FROM = "East 2nd Street,601"
-ADDRESS_TO = "13001 1st Street"
+ADDRESS_TO = "1300 1st Street"
 PHONE_NUMBER = "+1 123 123 1212"
 CARD_NUMBER = "1234 5678 9123 4567"
 CARD_CODE = "1111"
