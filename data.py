411b174241294dedae7cd40351047bb1dd2e7c8b
cameronhudson541 <cameronhudson541@gmail.com>
2025-12-09 14:10:03 -0600

    Update URBAN_ROUTES_URL to new endpoint

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = 'https://cnt-a84df0dc-98b1-43c0-992e-d45d6f505104.containerhub.tripleten-services.com'
+URBAN_ROUTES_URL = ' https://cnt-05fbd1e2-cd42-4794-b84d-30d8c4abfa5d.containerhub.tripleten-services.com'
 ADDRESS_FROM = "East 2nd Street,601"
 ADDRESS_TO = "1300 1st Street"
 PHONE_NUMBER = "+1 123 123 1212"
