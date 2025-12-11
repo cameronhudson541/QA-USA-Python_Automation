920136a0fd5920ef5951f84286bae5af194ca5d9
cameronhudson541 <cameronhudson541@gmail.com>
2025-12-11 10:52:00 -0600

    Update URLs and card number in data.py

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
-URBAN_ROUTES_URL = ' https://cnt-05fbd1e2-cd42-4794-b84d-30d8c4abfa5d.containerhub.tripleten-services.com'
+URBAN_ROUTES_URL = 'https://cnt-da9386f2-6179-4b25-a586-5aaac5508102.containerhub.tripleten-services.com'
 ADDRESS_FROM = "East 2nd Street,601"
 ADDRESS_TO = "1300 1st Street"
 PHONE_NUMBER = "+1 123 123 1212"
-CARD_NUMBER = "1234 5678 9123 4567"
+CARD_NUMBER = "1234 5678 9100"
 CARD_CODE = "1111"
 MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
